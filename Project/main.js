b130d5886807163b9a81d2ec400966a68ad3cc96
ashishugi <ashishkumarguptacse@gmail.com>
2020-04-09 12:26:09 +0530

    update 2

--- Project/main.js
+++ Project/main.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-app.use(express.static('view'));
+app.set('view engine','ejs');
+app.set('views','./view/pages');
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+"/view/index.html");              
+    res.render('index');
+});
+app.get('/about',(req,res)=>{
+    res.render('about');
 });
 app.listen(3000,()=>{
     console.log('server is running on port 3000 .....'); // type localhost:3000
